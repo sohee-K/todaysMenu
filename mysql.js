12d3824698caff3a132401ed80ec144a569b4c59
Sohee KWON <soheekwon@naver.com>
2020-10-02 03:29:17 +0900

    modify mysql.js

--- mysql.js
+++ mysql.js
@@ -1,9 +1,8 @@
-const express = require('express');
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
+    user: 'project',
     password: '123456',
     database: 'calendars'
 });
@@ -12,7 +11,9 @@ connection.connect();
 
 connection.query('select cd_kyganjee, cd_kmganjee, cd_kdganjee from calenda_data where cd_sy = "1998" and cd_sm = "10" and cd_sd = "17";', 
     (error, results, fields) => {
-        console.log(results[0].solution);
+        console.log(results[0].cd_kyganjee);
+        console.log(results[0].cd_kmganjee);
+        console.log(results[0].cd_kdganjee);
 });
 
 connection.end();
